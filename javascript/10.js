7b92018c2e833cd66e3ad09d28aafdfde37ecb62
Franco Davolio <francodavolio12@gmail.com>
2022-07-06 10:19:09 -0300

    creacion de la nueva rama

---
+++ javascript/10.js
@@ -0,0 +1,7 @@
+let num1 = prompt("Escribe un número");
+
+if (num1 % 2 === 0 , num1 % 3 === 0 , num1 % 5 === 0 , num1 % 7 === 0) {
+document.write("Es divisible por 2, 3, 5 o 7");
+} else {
+document.write("No es divisible ni por 2, ni por 3, ni por 5, ni por 7");
+}
---
+++ javascript/11.js
@@ -0,0 +1,18 @@
+let num1 = prompt("Escribe un número");
+
+if (num1 % 2 === 0 || num1 % 3 === 0 || num1 % 5 === 0 || num1 % 7 === 0) {
+if (num1 % 2 === 0) {
+document.write("Es divisible por 2. ");
+}
+if (num1 % 3 === 0) {
+document.write("Es divisible por 3. ");
+}
+if (num1 % 5 === 0) {
+document.write("Es divisible por 5. ");
+}
+if (num1 % 7 === 0) {
+document.write("Es divisible por 7. ");
+}
+} else {
+document.write("No es divisible ni por 2, ni por 3, ni por 5, ni por 7");
+}
